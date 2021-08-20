0e73066d9f91e45332260f785e5d13d4693839d5
kirileec <slk1133@qq.com>
2021-08-20 11:43:06 +0800

    add demo update README.md

---
+++ example/go.mod
@@ -0,0 +1,8 @@
+module test_kapi
+
+go 1.16
+
+require (
+	gitee.com/kirile/kapi v0.2.2
+	github.com/gin-gonic/gin v1.7.4
+)
---
+++ example/routers/gen_router.go
@@ -0,0 +1,11 @@
+//Package routers code generate by KApi on 2021-08-20 11:22:47. do not edit it.
+package routers
+
+import (
+	"gitee.com/kirile/kapi"
+)
+
+func init() {
+	kapi.SetVersion(1629429767)
+	kapi.AddGenOne("Hello/List", "/hello/list", []string{"get"})
+}
