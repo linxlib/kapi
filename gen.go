7b9adc50523510da2aa100f99221a93c8f769565
linx <slk1133@qq.com>
2021-10-27 16:30:21 +0800

    1

--- gen.go
+++ gen.go
@@ -2,6 +2,7 @@ package kapi
 
 import (
 	"bytes"
+	"encoding/gob"
 	"gitee.com/kirile/kapi/internal"
 	"os"
 	"os/exec"
@@ -14,6 +15,15 @@ import (
 var _mu sync.Mutex // protects the serviceMap
 var _once sync.Once
 var _genInfo genInfo
+
+func init() {
+	if internal.CheckFileIsExist("gen.gob") {
+		data := internal.ReadFile("gen.gob")
+		var buf = bytes.NewBuffer(data)
+		dec  := gob.NewDecoder(buf)
+		dec.Decode(&_genInfo)
+	}
+}
 
 // AddGenOne add one to base case
 func AddGenOne(handFunName, routerPath string, methods []string) {
@@ -54,7 +64,17 @@ func getStringList(list []string) string {
 func genOutPut(outDir, modFile string) {
 	_mu.Lock()
 	defer _mu.Unlock()
-	genCode(outDir, modFile) // gen .go file
+	//genCode(outDir, modFile) // gen .go file
+
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	_genInfo.Tm = time.Now().Unix()
+	err := encoder.Encode(_genInfo)
+	if err != nil {
+		_log.Error(err)
+		return
+	}
+	internal.WriteFile("gen.gob",buf.Bytes(),true)
 }
 
 
