300359dc1f5349c0118636ac49499bb448bbeb63
linx <slk1133@qq.com>
2021-10-27 16:41:24 +0800

    1

--- k/cmd/initialize.go
+++ k/cmd/initialize.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/mod/modfile"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
 var (
@@ -29,6 +30,35 @@ var (
    password = ""
    db = 1
 `
+	mainContent = `package main
+
+import (
+	"gitee.com/kirile/kapi"
+	"gitee.com/kirile/kapi/app"
+	"gitee.com/kirile/kapi/doc/swagger"
+)
+
+func main() {
+	//swagger.SetHost("")
+	swagger.SetBasePath("")
+	k := kapi.New(func(option *kapi.Option) {
+		option.SetNeedDoc(true)
+		option.SetDocName("%s")
+		option.SetDocDescription("%s api")
+		option.SetIsDebug(true)
+		option.SetPort(3080)
+		option.SetDocVersion("")
+		option.SetApiBasePath("/api")
+		//option.SetDocDomain("")
+		option.SetRedirectToDocWhenAccessRoot(true)
+	})
+	//k.RegisterRouter(new(api.MainController),new(api.CategoryController))
+
+	app.InitDB2()
+
+	k.Run()
+}
+`
 )
 
 func Initialize() {
@@ -51,7 +81,14 @@ func Initialize() {
 			_log.Println("写出config.toml")
 		}
 		if !gfile.Exists("api") {
+			_log.Println("创建api目录")
 			gfile.Mkdir("api")
+		}
+		if !gfile.Exists("main.go") {
+			ioutil.WriteFile("main.go",[]byte(mainContent),os.ModePerm)
+			_log.Println("写出main.go")
+			exec.Command("gofmt", "-l", "-w", "./").Output()
+			exec.Command("go mod tidy").Output()
 		}
 	} else {
 		_log.Println("go.mod不存在")
