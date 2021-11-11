a68e2e56ef9ff10ad2c4c08e8deb2123598fd07e
linx <slk1133@qq.com>
2021-11-11 17:27:47 +0800

    1

--- example/api/controller/hello.go
+++ example/api/controller/hello.go
@@ -9,11 +9,21 @@ import (
 type Hello struct {
 }
 
-type TestReq struct {
-	Page int `query:"page"` //页码
-	Size int `query:"size"` //页数量
+type PageSize struct {
+	Page int `query:"page" default:"1"`
+	Size int `query:"size" default:"15"`
 }
 
+func (p *PageSize) GetLimit() (int, int) {
+	return p.Size, (p.Page - 1) * p.Size
+}
+
+type TestReq struct {
+	PageSize
+	Name string `query:"name"` //名称查找
+}
+
+//List 获取列表
 // @GET /list
 func (h *Hello) List(c *kapi.Context, req *TestReq) {
 	list := make([]TestReq, 0)
@@ -24,8 +34,11 @@ func (h *Hello) List(c *kapi.Context, req *TestReq) {
 }
 
 // @GET /hello/list2
-func (h *Hello) List2(c *kapi.Context, req *TestReq) {
-
+func (h *Hello) List2(c *kapi.Context, req *TestReq) ([]TestReq, error) {
+	list := make([]TestReq, 0)
+	list = append(list, *req)
+	//count := int64(30)
+	return list, nil
 }
 
 func init() {
