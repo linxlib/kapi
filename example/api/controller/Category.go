903c0c735c70ecf6c9307452e3c1814630b0ed9a
linx <slk1133@qq.com>
2022-05-21 17:12:21 +0800

    update example

--- example/api/controller/Category.go
+++ example/api/controller/Category.go
@@ -6,7 +6,9 @@ import (
 	"test_kapi/lib"
 )
 
-//@AUTH Authorzation
+//CategoryController hhh
+//@TAG hudhushd和上面这行同时存在时，这个优先 对了 注解后有空格的话，空格后的内容会被无视
+//@AUTH Authorization 表示下面的每个方法都会需要Authorization这个Header Authorization是默认 所以也可以不写
 type CategoryController struct {
 }
 
@@ -56,9 +58,9 @@ func (p *CategoryController) DelCategory(c *kapi.Context, req *DelCategoryReq) {
 }
 
 //TestAnyMethod
-//@GET /TestAnyMethod
-//@POST /TestAnyMethod
-//@PUT /TestAnyMethod
+//@GET /TestAnyMethod1
+//@POST /TestAnyMethod1 但是上面的不能不写 ^_^
+//@PUT /TestAnyMethod2 只会以最下面这个为准 以后会更新 让每个都不一样
 func (p *CategoryController) TestAnyMethod(c *kapi.Context, req *DelCategoryReq) {
 
 }
